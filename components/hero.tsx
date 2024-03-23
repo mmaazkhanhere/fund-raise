8d7e9ff989f96f51f38275c85d91e4b12bf2ba1f
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-03-23 10:49:30 +0500

    mobile hero completed

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,34 +4,66 @@ type Props = {}
 
 const Hero = (props: Props) => {
     return (
-        <section
-            style={{
-                backgroundImage: 'url("/hero.jpg")',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center bottom',
-            }}
-            className='w-full h-[700px] flex items-center justify-start'
-        >
-            <div
-                className='max-w-7xl w-full mx-auto text-[#f8f8f2] flex flex-col 
-                items-start justify-center px-4'
+        <React.Fragment>
+            <section
+                style={{
+                    backgroundImage: 'url("/hero.jpg")',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center bottom',
+                }}
+                className='w-full h-[700px] hidden md:flex items-center justify-start'
             >
-                <div className='flex flex-col max-w-3xl gap-y-2'>
-                    <h1
-                        className='text-5xl lg:text-8xl font-black uppercase leading-tight'
-                    >
-                        Unite for a Greener Tomorrow
-                    </h1>
-                    <p
-                        className='text-lg lg:text-2xl uppercase font-mono '
-                    >
-                        Fund the future save the future
-                    </p>
-                </div>
+                <div
+                    className='max-w-7xl w-full mx-auto text-[#f8f8f2] flex flex-col 
+                items-start justify-center px-4'
+                >
+                    <div className='flex flex-col max-w-3xl gap-y-2'>
+                        <h1
+                            className='md:text-6xl lg:text-8xl font-black uppercase leading-tight'
+                        >
+                            Unite for a Greener Tomorrow
+                        </h1>
+                        <p
+                            className='text-xl lg:text-2xl uppercase font-mono '
+                        >
+                            Fund the future save the future
+                        </p>
+                    </div>
 
-            </div>
-        </section>
+                </div>
+            </section>
+
+            <section
+                style={{
+                    backgroundImage: 'url("/hero-mobile-2.jpg")',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center',
+                }}
+                className='w-full h-[700px] flex md:hidden items-center justify-start'
+            >
+                <div
+                    className='max-w-7xl w-full mx-auto text-[#0f2417] flex flex-col 
+                items-start justify-center px-4'
+                >
+                    <div className='flex flex-col max-w-3xl gap-y-2'>
+                        <h1
+                            className='text-5xl font-black uppercase leading-tight'
+                        >
+                            Unite for a Greener Tomorrow
+                        </h1>
+                        <p
+                            className='text-lg uppercase font-mono '
+                        >
+                            Fund the future save the future
+                        </p>
+                    </div>
+
+                </div>
+            </section>
+        </React.Fragment>
+
     )
 }
 
